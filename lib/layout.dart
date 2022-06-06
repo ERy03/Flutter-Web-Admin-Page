2feffd88b774f81d434af196a442500eb196260e
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 12:50:53 +0900

    medium screen

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'helpers/responsiveness.dart';
 import 'widgets/large_screen.dart';
+import 'widgets/medium_screen.dart';
 import 'widgets/small_screen.dart';
 
 class SiteLayout extends StatelessWidget {
@@ -13,7 +15,11 @@ class SiteLayout extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: SmallScreen(),
+      body: ResponsiveWidget(
+        largeScreen: LargeScreen(),
+        smallScreen: SmallScreen(),
+        mediumScreen: MediumScreen(),
+      ),
     );
   }
 }
