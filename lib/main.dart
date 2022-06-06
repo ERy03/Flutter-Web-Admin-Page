b25d2b84cbd01ed43aa88b2e44970d1767ab3c3d
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 11:45:22 +0900

    main setup

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "管理画面",
+      home: Center(
+        child: Container(
+          child: const Text("Hello"),
+        ),
+      ),
+    );
+  }
+}
