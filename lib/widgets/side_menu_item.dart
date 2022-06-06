d1a6173d8454ad253a4de3ca72dec65c81f9b06f
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 18:39:53 +0900

    side menu item logic

---
+++ lib/widgets/side_menu_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SideMenuItem extends StatelessWidget {
+  final String itemName;
+  final Function onTap;
+  const SideMenuItem({Key? key, required this.itemName, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
