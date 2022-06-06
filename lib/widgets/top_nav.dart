9ea7d71a4d1e02f78b1e632c607e9ee9906a926a
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 15:04:23 +0900

    top nav

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/helpers/responsiveness.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                )
+              ],
+            )
+          : IconButton(
+              onPressed: (() {
+                key.currentState?.openDrawer();
+              }),
+              icon: const Icon(Icons.menu),
+            ),
       elevation: 0,
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
     );
