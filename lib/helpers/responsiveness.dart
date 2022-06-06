10b404a32ddb7bb006fccda2619b5f3cc37feb9e
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 12:20:39 +0900

    helpers : responsivewidget

--- lib/helpers/responsiveness.dart
+++ lib/helpers/responsiveness.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class ResponsiveWidget extends StatelessWidget {
   const ResponsiveWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
