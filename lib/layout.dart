990d4d6e65e7d641c74de1b62e64a06c2abf007e
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 15:56:38 +0900

    layout: passing key scaffold key

--- lib/layout.dart
+++ lib/layout.dart
@@ -13,6 +13,7 @@ class SiteLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: topNavigationBar(context, scaffoldKey),
       drawer: Drawer(),
       body: ResponsiveWidget(
