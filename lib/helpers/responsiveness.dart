a6482817da3d1fef60481bad543f129e63ac799a
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 12:22:33 +0900

    helpers : added screen size break points

--- lib/helpers/responsiveness.dart
+++ lib/helpers/responsiveness.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const int largeScreenSize = 1366;
+const int mediumScreenSize = 768;
+const int smallScreenSize = 360;
+const int customScreenSize = 1100;
+
 class ResponsiveWidget extends StatelessWidget {
   const ResponsiveWidget({Key? key}) : super(key: key);
 
