c30cf40cd2dc150b8d95257605f0d911e0bb50b7
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 18:50:36 +0900

    side menu item logic

--- lib/widgets/side_menu_item.dart
+++ lib/widgets/side_menu_item.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/helpers/responsiveness.dart';
+import 'package:flutter_web_admin_page/widgets/horizontal_menu_item.dart';
+import 'package:flutter_web_admin_page/widgets/vertical_menu_item.dart';
 
 class SideMenuItem extends StatelessWidget {
   final String itemName;
-  final Function onTap;
+  final void Function() onTap;
   const SideMenuItem({Key? key, required this.itemName, required this.onTap})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (ResponsiveWidget.isCustomScreen(context))
+      return VerticalMenuItem(
+        itemName: itemName,
+        onTap: onTap,
+      );
+    return HorizontalMenuItem(
+      itemName: itemName,
+      onTap: onTap,
+    );
   }
 }
