f9c248db3e1ab3a09221c3a00027861bf910d04a
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 15:16:37 +0900

    custom text

--- lib/widgets/custom_text.dart
+++ lib/widgets/custom_text.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CustomText extends StatelessWidget {
-  const CustomText({Key? key}) : super(key: key);
+  final String text;
+  final double? size;
+  final Color? color;
+  final FontWeight? weight;
+
+  const CustomText(
+      {Key? key, required this.text, this.size, this.color, this.weight})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: size ?? 16,
+        color: color ?? Colors.black,
+        fontWeight: weight ?? FontWeight.normal,
+      ),
+    );
   }
 }
