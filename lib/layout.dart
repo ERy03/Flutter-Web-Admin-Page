9bd1fa9efef1d1f819c1b4ba186b3327780c4058
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 12:15:29 +0900

    created small screen widget

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'widgets/large_screen.dart';
+import 'widgets/small_screen.dart';
 
 class SiteLayout extends StatelessWidget {
   const SiteLayout({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class SiteLayout extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: LargeScreen(),
+      body: SmallScreen(),
     );
   }
 }
