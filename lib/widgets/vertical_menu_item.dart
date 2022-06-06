89f77c001a2ab3e33181b0f5f22454b93e523e67
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 18:34:26 +0900

    vertical menu item obx

--- lib/widgets/vertical_menu_item.dart
+++ lib/widgets/vertical_menu_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_admin_page/constants/controllers.dart';
+import 'package:get/get.dart';
+
+import '../constants/style.dart';
 
 class VerticalMenuItem extends StatelessWidget {
   final String itemName;
@@ -18,6 +21,27 @@ class VerticalMenuItem extends StatelessWidget {
             ? menuController.onHover(itemName)
             : menuController.onHover("not hovering");
       },
+      child: Obx(() => Container(
+            color: menuController.isHovering(itemName)
+                ? lightGrey.withOpacity(.1)
+                : Colors.transparent,
+            child: Row(
+              children: [
+                Visibility(
+                  visible: menuController.isHovering(itemName) ||
+                      menuController.isActive(itemName),
+                  maintainSize: true,
+                  maintainAnimation: true,
+                  maintainState: true,
+                  child: Container(
+                    width: 3,
+                    height: 72,
+                    color: dark,
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
