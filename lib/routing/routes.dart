87e1e4e468a26eb175d66c83dfe40a56711c2152
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 16:09:30 +0900

    route list

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -2,3 +2,10 @@ const overViewPageRoute = "Overview";
 const driversPageRoute = "Drivers";
 const clientsPageRoute = "Clients";
 const authenticationRoute = "Authentication";
+
+List sideMenuItems = [
+  overViewPageRoute,
+  driversPageRoute,
+  clientsPageRoute,
+  authenticationRoute,
+];
