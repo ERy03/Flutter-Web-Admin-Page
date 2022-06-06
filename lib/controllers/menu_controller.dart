fa0a368bb43c1b965277f356a6bbf439985531e5
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 16:50:51 +0900

    return icon for method

--- lib/controllers/menu_controller.dart
+++ lib/controllers/menu_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_web_admin_page/routing/routes.dart';
 import 'package:get/get.dart';
 
@@ -17,4 +18,33 @@ class MenuController extends GetxController {
   isActive(String itemName) => activeItem.value == itemName;
 
   isHovering(String itemName) => hoverItem.value == itemName;
+
+  Widget returnIconFor(String itemName) {
+    switch (itemName) {
+      case overViewPageRoute:
+        return _customIcon(
+          Icons.trending_up,
+          itemName,
+        );
+      case driversPageRoute:
+        return _customIcon(
+          Icons.drive_eta,
+          itemName,
+        );
+      case clientsPageRoute:
+        return _customIcon(
+          Icons.people_alt_outlined,
+          itemName,
+        );
+      case authenticationPageRoute:
+        return _customIcon(
+          Icons.exit_to_app,
+          itemName,
+        );
+      default:
+        return _customIcon(Icons.exit_to_app, itemName);
+    }
+  }
+
+  Widget _customIcon(IconData trending_up, String itemName) {}
 }
--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,11 +1,11 @@
 const overViewPageRoute = "Overview";
 const driversPageRoute = "Drivers";
 const clientsPageRoute = "Clients";
-const authenticationRoute = "Authentication";
+const authenticationPageRoute = "Authentication";
 
 List sideMenuItems = [
   overViewPageRoute,
   driversPageRoute,
   clientsPageRoute,
-  authenticationRoute,
+  authenticationPageRoute,
 ];
