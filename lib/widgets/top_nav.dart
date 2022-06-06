483925997b5d9368213299d5f181380b8c1f0fce
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-06 15:19:56 +0900

    top nav: title

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
 import 'package:flutter_web_admin_page/helpers/responsiveness.dart';
+import 'package:flutter_web_admin_page/widgets/custom_text.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
-      leading: !ResponsiveWidget.isSmallScreen(context)
-          ? Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(left: 14),
-                  child: Image.asset(
-                    "assets/icons/logo.png",
-                    width: 28,
-                  ),
-                )
-              ],
+        leading: !ResponsiveWidget.isSmallScreen(context)
+            ? Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 14),
+                    child: Image.asset(
+                      "assets/icons/logo.png",
+                      width: 28,
+                    ),
+                  )
+                ],
+              )
+            : IconButton(
+                onPressed: (() {
+                  key.currentState?.openDrawer();
+                }),
+                icon: const Icon(Icons.menu),
+              ),
+        elevation: 0,
+        title: Row(
+          children: [
+            Visibility(
+              child: CustomText(
+                text: "hello",
+                color: lightGrey,
+                size: 20,
+                weight: FontWeight.bold,
+              ),
             )
-          : IconButton(
-              onPressed: (() {
-                key.currentState?.openDrawer();
-              }),
-              icon: const Icon(Icons.menu),
-            ),
-      elevation: 0,
-      // backgroundColor: Colors.white,
-    );
+          ],
+        )
+        // backgroundColor: Colors.white,
+        );
