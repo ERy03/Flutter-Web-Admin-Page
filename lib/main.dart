799c2909eafa2f863e12bc8c7a14deefdb4adfa0
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 12:06:37 +0900

    navigation controller main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_admin_page/constants/style.dart';
 import 'package:flutter_web_admin_page/controllers/menu_controller.dart';
+import 'package:flutter_web_admin_page/controllers/navigation_controller.dart';
 import './layout.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   Get.put(MenuController());
+  Get.put(NavigationController());
   runApp(MyApp());
 }
 
