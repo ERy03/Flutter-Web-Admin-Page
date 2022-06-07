5b70856e68ac4060fd228e8d4d90db5d3686830d
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 10:26:27 +0900

    side menu

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
+import 'package:flutter_web_admin_page/helpers/responsiveness.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double width = MediaQuery.of(context).size.width;
+    return Container(
+      color: light,
+      child: ListView(
+        children: [
+          if (ResponsiveWidget.isSmallScreen(context))
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: width / 48,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        right: 12,
+                      ),
+                      child: Image.asset("assets/icons/logo.png"),
+                    ),
+                  ],
+                ),
+              ],
+            )
+        ],
+      ),
+    );
   }
 }
