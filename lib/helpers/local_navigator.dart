e094f95b1a65f86ddf38aec223d1946a2fbf7d52
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 12:10:18 +0900

    local navigator

---
+++ lib/helpers/local_navigator.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/controllers.dart';
+import 'package:flutter_web_admin_page/routing/routes.dart';
+
+Navigator localNavigator() => Navigator(
+      key: navigationController.navigationKey,
+      initialRoute: overViewPageRoute,
+    );
