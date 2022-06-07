332938e8e596595d22172d6066d67161c32fae34
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 15:14:55 +0900

    info card small

--- lib/pages/overview/widgets/info_card_small.dart
+++ lib/pages/overview/widgets/info_card_small.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
 
 class MyWidget extends StatelessWidget {
   final String? title;
@@ -13,7 +14,19 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: InkWell(),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: isActive ? active : lightGrey,
+                width: 0.5,
+              )),
+        ),
+      ),
     );
   }
 }
