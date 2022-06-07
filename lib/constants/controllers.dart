10ad9152ee238e779d2e8619d2150508c8a9f97c
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 12:06:01 +0900

    navigation controller instance

--- lib/constants/controllers.dart
+++ lib/constants/controllers.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_web_admin_page/controllers/menu_controller.dart';
+import 'package:flutter_web_admin_page/controllers/navigation_controller.dart';
 
 MenuController menuController = MenuController.instance;
+NavigationController navigationController = NavigationController.instance;
