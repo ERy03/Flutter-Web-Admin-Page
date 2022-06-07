e42b5f60524e997fad31d5a94cbb4d893a7df0db
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 17:05:13 +0900

    revenue info

---
+++ lib/pages/overview/widgets/revenue_info.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RevenueInfo extends StatelessWidget {
+  const RevenueInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
