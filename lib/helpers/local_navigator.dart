9eb38efb81d4412fc03151461431dd461910c9be
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 12:28:10 +0900

    on generate route

--- lib/helpers/local_navigator.dart
+++ lib/helpers/local_navigator.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_admin_page/constants/controllers.dart';
+import 'package:flutter_web_admin_page/routing/router.dart';
 import 'package:flutter_web_admin_page/routing/routes.dart';
 
 Navigator localNavigator() => Navigator(
       key: navigationController.navigationKey,
       initialRoute: overViewPageRoute,
+      onGenerateRoute: generateRoute,
     );
