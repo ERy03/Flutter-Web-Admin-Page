a0c7a0a43fcb02db4877d4f5d5dc44809c8f2566
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 17:23:45 +0900

    revenue section large

--- lib/pages/overview/widgets/revenue_section_large.dart
+++ lib/pages/overview/widgets/revenue_section_large.dart
@@ -4,5 +4,14 @@ class RevenueSectionLarge extends StatelessWidget {
   const RevenueSectionLarge({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      margin: EdgeInsets.symmetric(vertical: 30),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
 }
