bc389f02ada60c70f7db96a0ec5773643f75c1ae
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 12:26:22 +0900

    route

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/pages/clients/clients.dart';
+import 'package:flutter_web_admin_page/pages/drivers/drivers.dart';
+import 'package:flutter_web_admin_page/pages/overview/overview.dart';
+import 'package:flutter_web_admin_page/routing/routes.dart';
 
-Route<dynamic> generateRoute(RouteSettings) {}
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case overViewPageRoute:
+      return _getPageRoute(OverViewPage());
+    case driversPageRoute:
+      return _getPageRoute(DriversPage());
+    case clientsPageRoute:
+      return _getPageRoute(ClientsPage());
+    default:
+      return _getPageRoute(OverViewPage());
+  }
+}
 
 PageRoute _getPageRoute(Widget child) {
   return MaterialPageRoute(builder: (context) => child);
