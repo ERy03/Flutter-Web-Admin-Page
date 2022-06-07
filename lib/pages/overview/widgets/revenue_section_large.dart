61fbd91d3b29bbd53390b15a464548540ed4866c
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 17:27:49 +0900

    revenue section large box shadow

--- lib/pages/overview/widgets/revenue_section_large.dart
+++ lib/pages/overview/widgets/revenue_section_large.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
 
 class RevenueSectionLarge extends StatelessWidget {
   const RevenueSectionLarge({Key? key}) : super(key: key);
@@ -6,12 +7,18 @@ class RevenueSectionLarge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(24),
-      margin: EdgeInsets.symmetric(vertical: 30),
+      padding: const EdgeInsets.all(24),
+      margin: const EdgeInsets.symmetric(vertical: 30),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 6),
+              color: lightGrey.withOpacity(0.1),
+              blurRadius: 12,
+            )
+          ]),
     );
   }
 }
