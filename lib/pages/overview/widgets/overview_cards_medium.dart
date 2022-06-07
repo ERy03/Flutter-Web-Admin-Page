6028551d9ffcd30ea983f49010ede0ccc582c2c0
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 15:45:50 +0900

    overview cards medium

---
+++ lib/pages/overview/widgets/overview_cards_medium.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class OverviewCardMediumScreenSize extends StatelessWidget {
+  const OverviewCardMediumScreenSize({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
