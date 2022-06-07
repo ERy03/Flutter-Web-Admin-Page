c873235bb9437bd4da4a0c631b83a6908e3d15d5
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 14:54:22 +0900

    overview info card

--- lib/pages/overview/widgets/info_card.dart
+++ lib/pages/overview/widgets/info_card.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
 
 class InfoCard extends StatelessWidget {
   final String title;
   final String value;
   final Color topColorl;
   final bool isActive;
-  final Function onTap;
+  final void Function() onTap;
 
   const InfoCard(
       {Key? key,
@@ -17,5 +18,26 @@ class InfoCard extends StatelessWidget {
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          height: 136,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                offset: const Offset(0, 6),
+                color: lightGrey.withOpacity(0.1),
+                blurRadius: 12,
+              )
+            ],
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
 }
