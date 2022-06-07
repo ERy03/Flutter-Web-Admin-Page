36a9e7c3fb8aa52703ea2ee077905e3a74a7dab9
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 18:01:11 +0900

    revenue section small created

---
+++ lib/pages/overview/widgets/revenue_section_small.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RevenueSectionSmall extends StatelessWidget {
+  const RevenueSectionSmall({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
