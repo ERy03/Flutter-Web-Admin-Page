9b8b2ee74465d233c77c3038e7e964e0bd01b1d8
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 18:04:50 +0900

    revenue section small custom text

--- lib/pages/overview/widgets/revenue_section_small.dart
+++ lib/pages/overview/widgets/revenue_section_small.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
+import 'package:flutter_web_admin_page/widgets/custom_text.dart';
 
 class RevenueSectionSmall extends StatelessWidget {
   const RevenueSectionSmall({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      margin: const EdgeInsets.symmetric(vertical: 30),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 6),
+            color: lightGrey.withOpacity(0.1),
+            blurRadius: 12,
+          ),
+        ],
+        border: Border.all(
+          color: lightGrey,
+          width: 0.5,
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 260,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomText(
+                  text: "Revenue Chart",
+                  size: 20,
+                  weight: FontWeight.bold,
+                  color: lightGrey,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
