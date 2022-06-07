e4d2e2935970ce57a370d1fafff9cc435fedf032
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-07 17:09:56 +0900

    revenue info

--- lib/pages/overview/widgets/revenue_info.dart
+++ lib/pages/overview/widgets/revenue_info.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_admin_page/constants/style.dart';
 
 class RevenueInfo extends StatelessWidget {
-  const RevenueInfo({Key? key}) : super(key: key);
+  final String title;
+  final String amount;
+
+  const RevenueInfo({Key? key, required this.title, required this.amount})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "$title \n\n",
+              style: TextStyle(
+                color: lightGrey,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
